eab941b57fd5d058fdb978c3eac6156f267d2150
amellsntya <amellsintyaa09@gmail.com>
2024-03-05 17:24:51 +0700

    test

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost","root","","informasisekolah");
+$conn = mysqli_connect("localhost","root","","multi_user");
 
 if($conn->connect_errno){
     die("koneksi gagal : ".$conn->error);
